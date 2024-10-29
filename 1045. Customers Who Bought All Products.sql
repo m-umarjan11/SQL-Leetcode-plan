b47930dfe63cf8dc86d5dfa6423a177d32ac2fa7
Muhammad Umar Jan <104164416+m-umarjan11@users.noreply.github.com>
2024-10-29 10:16:49 -0700

    SQL

---
+++ 1045. Customers Who Bought All Products.sql
@@ -0,0 +1,34 @@
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| customer_id | int     |
+| product_key | int     |
++-------------+---------+
+This table may contain duplicates rows. 
+customer_id is not NULL.
+product_key is a foreign key (reference column) to Product table.
+ 
+
+Table: Product
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_key | int     |
++-------------+---------+
+product_key is the primary key (column with unique values) for this table.
+ 
+
+Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+
+Return the result table in any order.
+
+SOLUTION:
+    # Write your MySQL query statement below
+
+SELECT  customer_id 
+FROM Customer
+ GROUP BY customer_id
+HAVING COUNT(distinct product_key) = (SELECT COUNT(product_key) FROM Product)
