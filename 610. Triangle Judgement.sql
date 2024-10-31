b03fe9e8f8621554fd8c83d0c1ae3ac86953eb6a
Muhammad Umar Jan <104164416+m-umarjan11@users.noreply.github.com>
2024-10-31 09:36:59 -0700

    SQL

---
+++ 610. Triangle Judgement.sql
@@ -0,0 +1,21 @@
+able: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+SOLUTION:
+    # Write your MySQL query statement below
+SELECT *, IF(x+y>z AND y+z>x AND x+z>y,"Yes","No") AS triangle 
+FROM Triangle 
