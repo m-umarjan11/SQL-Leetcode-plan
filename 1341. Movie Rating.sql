91975f1a9d111285e8b61c490fb34526e3d69f97
Muhammad Umar Jan <104164416+m-umarjan11@users.noreply.github.com>
2024-11-28 03:47:36 -0800

    SQL

---
+++ 1341. Movie Rating.sql
@@ -0,0 +1,63 @@
+able: Movies
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| title         | varchar |
++---------------+---------+
+movie_id is the primary key (column with unique values) for this table.
+title is the name of the movie.
+ 
+
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+The column 'name' has unique values.
+Table: MovieRating
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| user_id       | int     |
+| rating        | int     |
+| created_at    | date    |
++---------------+---------+
+(movie_id, user_id) is the primary key (column with unique values) for this table.
+This table contains the rating of a movie by a user in their review.
+created_at is the user's review date. 
+ 
+
+Write a solution to:
+
+Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+'
+SOLUTION:
+    # Write your MySQL query statement below
+(SELECT name as results
+FROM Users 
+INNER JOIN MovieRating USING(user_id)
+GROUP BY user_id
+ORDER BY COUNT(rating) DESC, name
+LIMIT  1
+)
+
+UNION ALL
+
+(
+SELECT title AS results
+FROM Movies
+INNER JOIN MovieRating USING(movie_id)
+WHERE MONTH(created_at) = '02' AND YEAR(created_at) = '2020'
+GROUP BY movie_id
+ORDER BY AVG(rating) DESC ,title
+limit 1
+)
